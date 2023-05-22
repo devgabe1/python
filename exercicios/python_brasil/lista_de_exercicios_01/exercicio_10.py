a8015826dc1b501e87bb0c9727e917a0e65fcf40
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-05-22 20:00:20 -0300

    Adicionado exercício 10  da lista 01

---
+++ exercicios/python_brasil/lista_de_exercicios_01/exercicio_10.py
@@ -0,0 +1,5 @@
+#encoding: utf-8
+print("Conversor graus Celsius")
+temp = float(input("Insira a temperatura em graus: "))
+temp = ((temp * 9) / 5) + 32
+print(f"A temperatura convertida em Fahrenheit é: {temp:.1f}ºF")
--- exercicios/python_brasil/lista_de_exercicios_01/exercício_09.py
+++ exercicios/python_brasil/lista_de_exercicios_01/exercício_09.py
@@ -2,4 +2,4 @@
 print("Conversor graus Fahrenheit")
 temp = float(input("Insira a temperatura em graus Fahrenheit: "))
 temp = 5 * ((temp - 32) / 9)
-print(f"A temperatura convertida em Celsius é: {temp:.2f}ºC")
+print(f"A temperatura convertida em Celsius é: {temp:.1f}ºC")
