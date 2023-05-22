ad26c3f885c6d661c41259a855f46dc181414f9d
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-05-22 19:34:07 -0300

    Adicionado exercício 06 da lista 01

---
+++ exercicios/python_brasil/lista_de_exercicios_01/exercicio_06.py
@@ -0,0 +1,5 @@
+# encoding: utf-8
+print("Calcular a área de um círculo")
+raio = int(input("Insira o raio do círculo: "))
+raio = (raio * raio) * 3.14
+print (f"A área do círculo é igual a: {raio}")
