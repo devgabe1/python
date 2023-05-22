77ee0e92172f042f515030fc783d6207f944172a
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-05-21 23:01:26 -0300

    Criado pasta de lista de exercicios 01

---
+++ lista_de_exercicios_01/exercicio_01.py
@@ -0,0 +1,4 @@
+# encoding: utf-8
+num01 = int(input)('Insira um número: ')
+num02 = int(input)('Insira um número: ')
+print(num01 + num02)
---
+++ lista_de_exercicios_01/exercicio_02.py
@@ -0,0 +1,3 @@
+# encoding: utf-8
+num = input('Insira um número: ')
+print(f'O número informado foi {num}')
