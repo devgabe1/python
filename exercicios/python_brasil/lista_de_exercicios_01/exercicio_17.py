8be66192a8110aae06a68e8e8200b65e8c9ada95
Gabriel Schultz Vasconcelos <gabrielschultz2807@gmail.com>
2023-05-23 17:50:55 -0300

    Adicionado exercicio 17 da lista 01

---
+++ exercicios/python_brasil/lista_de_exercicios_01/exercicio_17.py
@@ -0,0 +1,8 @@
+#encoding: utf-8
+
+print("Cálculo de latas e galões de tinta")
+tamanho = float(input("Insira em metros quadrados, o tamanho da área a ser pintada: "))
+litros_tinta = tamanho / 6
+litros_lata = 18
+litros_galao = 3.6
+
