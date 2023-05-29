d709eb4682f0997e081bdc0cc031437c91c286c1
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-05-29 19:15:35 -0300

    Adicionado exercício 01 da lista 02

--- exercicios/python_brasil/lista_de_exercicios_02/exercicio_01.py
+++ exercicios/python_brasil/lista_de_exercicios_02/exercicio_01.py
@@ -1,15 +1,18 @@
 #encoding: utf-8
 import os
-os.system ("cls")
-os.system ("clear")
+os.system("cls")
+os.system("clear")
 
 print("Calculando número maior.")
-num1 = int(input("Insira um valor: "))
-num2 = int(input("Insira outro valor: "))
+num1 = float(input("Insira um valor: "))
+num2 = float(input("Insira outro valor: "))
 
 if num1 > num2:
     num_maior = num1
+
+elif num2 == num1:
+    print("Os valores são iguais.")
+
 else:
     num_maior = num2
-
-print(f"O valor maior é {num_maior:.0f}")
+    print(f"O valor maior é {num_maior:.1f}")
