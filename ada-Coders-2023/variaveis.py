5e0cc5d71dc567a10053734e08ebc8ffc28b11ff
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-11-12 06:43:26 -0300

    Exercício sobre variáveis

---
+++ ada-Coders-2023/variaveis.py
@@ -0,0 +1,14 @@
+
+idade = 20
+altura = 1.70
+nome = 'Gabriel Schultz'
+estudando = True
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudando))
+
+linguagem = input('Qual é a linguagem de programação que você está estudando? ')
+
+print('A linguagem que você está estudando é', linguagem)
