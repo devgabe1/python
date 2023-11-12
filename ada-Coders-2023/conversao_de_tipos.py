96fb2864d167504cef8af4f860c00325362b51ec
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-11-12 06:55:32 -0300

    Conversão de variaveis

---
+++ ada-Coders-2023/conversao_de_tipos.py
@@ -0,0 +1,8 @@
+idade = '26'
+print(idade, type(idade))
+
+idade_inteira = int(idade)
+print(idade_inteira, type(idade_inteira))
+
+altura = float(input('Informe a sua altura: '))
+print(altura, type(altura))
