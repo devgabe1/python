91c3bfa232840969b600f2b5c8d97be62933f936
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-11-12 08:21:43 -0300

    Listas

---
+++ ada-Coders-2023/listas.py
@@ -0,0 +1,17 @@
+lista = [10, 'Gabriel', 3.1415, True]
+
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+
+print(lista[-1])
+
+print(lista[0:3])
+print(lista[0:])
+print(lista[0:4:2])
+
+for c in lista:
+    print(c)
+
+print('Comprimento da lista:', len(lista))
