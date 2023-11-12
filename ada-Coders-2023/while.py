42cd32076d1809301f65d2cbc9d0735a5ac74507
Gabriel Schultz <gabrielschultz2807@gmail.com>
2023-11-12 07:58:48 -0300

    Laço for

---
+++ ada-Coders-2023/while.py
@@ -0,0 +1,7 @@
+numero_sorteado = 5
+
+numero_escolhido = int(input('Informe umm númeroentre 1 e 20: '))
+
+while numero_escolhido != numero_sorteado:
+    print('Você errou o número, tente novamente...')
+    numero_escolhido = int(input('Informe umm númeroentre 1 e 20: '))
